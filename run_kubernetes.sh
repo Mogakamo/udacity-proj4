e2d7b691cf45cda0090f70c68591b94e4d76e2f4
Mogakamo <mogaka.amo254@gmail.com>
2022-08-11 11:56:29 +0300

    build: run_kubertenes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,21 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+# dockerpath='mogakadev20/microservices'
+dockerpath='mogakadev20/microservices'
+
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run microservices --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward deployment/microservices 8000:80
 
+# Get logs
+kubectl logs `kubectl get pods -o=name`
